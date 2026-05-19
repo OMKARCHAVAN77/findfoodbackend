142be3f7345703d6ae064f69d319589424f4dc7f
Omkar Chavan <chavanomi07@gmail.com>
2026-05-19 15:38:21 +0530

    fix: all middleware and controller final

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,46 +1,35 @@
 const jwt = require('jsonwebtoken')
 
+const authMiddleware = async(req, res, next) => {
+  try {
+    // Read from Authorization header (Bearer token)
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-const authMiddleware = async(req,res,next)=>{
-    try {
-        
-        const token = req.cookies?.token
+    console.log("token", token);
 
-        console.log("token",token);
-
-        if(!token){
-            return res.status(400).json({
-                success:false,
-                msg:"User is not log in.."
-            })
-        }
-
-        jwt.verify(token,process.env.Token_key,(err,decode)=>{
-            if(err){
-                console.log(err)
-                return res.status(400).json({
-                    success: false,
-                    msg: "Invalid token"
-                })
-            }
-
-            res.status(200).json({
-                success:true,
-                msg:"User is login",
-                data: decode
-            })
-            req.data = decode
-
-            // console.log(decode);
-
-        })
-
-        next()
-
-    } catch (error) {
-        console.log(error);
+    if(!token) {
+      return res.status(400).json({
+        success: false,
+        msg: "User is not log in.."
+      })
     }
-}
 
+    jwt.verify(token, process.env.Token_key, (err, decode) => {
+      if(err) {
+        console.log(err)
+        return res.status(400).json({
+          success: false,
+          msg: "Invalid token"
+        })
+      }
+      req.data = decode;
+      next();
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = authMiddleware
--- middlewares/messFormAccessMiddleware.js
+++ middlewares/messFormAccessMiddleware.js
@@ -1,38 +1,35 @@
 const jwt = require('jsonwebtoken')
 
+const authMiddleware = async(req, res, next) => {
+  try {
+    // Read from Authorization header (Bearer token)
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-const messFormAccessMiddleware = async(req,res,next)=>{
-    try {
-        
-        const token = req.cookies?.token
+    console.log("token", token);
 
-        if(!token){
-            return res.status(400).json({
-                success:false,
-                msg:"User is not log in.."
-            })
-        }
-
-        jwt.verify(token,process.env.Token_key,(err,decode)=>{
-            if(err){
-                console.log(err)
-                return res.status(400).json({
-                    success: false,
-                    msg: "Invalid token"
-                })
-            }
-            req.data = decode
-
-            // console.log(decode);
-
-        })
-
-        next()
-
-    } catch (error) {
-        console.log(error);
+    if(!token) {
+      return res.status(400).json({
+        success: false,
+        msg: "User is not log in.."
+      })
     }
+
+    jwt.verify(token, process.env.Token_key, (err, decode) => {
+      if(err) {
+        console.log(err)
+        return res.status(400).json({
+          success: false,
+          msg: "Invalid token"
+        })
+      }
+      req.data = decode;
+      next();
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-module.exports = messFormAccessMiddleware
+module.exports = authMiddleware
