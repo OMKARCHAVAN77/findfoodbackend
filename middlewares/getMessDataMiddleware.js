cec2c09148f32d6451adcbdfe7807e13f566583b
Omkar Chavan <chavanomi07@gmail.com>
2026-05-19 15:46:49 +0530

    fix: getMessDataMiddleware read Bearer token

--- middlewares/getMessDataMiddleware.js
+++ middlewares/getMessDataMiddleware.js
@@ -1,30 +1,33 @@
 const jwt = require('jsonwebtoken')
 
-const getMessDataMiddleware = async(req,res,next)=>{
-    try {
-        
-        const token = req.cookies?.token
+const getMessDataMiddleware = async(req, res, next) => {
+  try {
+    // ✅ Read from Authorization header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-        if(!token){
-            return res.status(400).json({
-                success:false,
-                msg:"User is not log in.."
-            })
-        }
-
-        jwt.verify(token,process.env.Token_key,(err,decode)=>{
-            if(err){
-                console.log(err);
-            }
-
-            req.data = decode
-        })
-
-        next()
-
-    } catch (error) {
-        console.log(error);
+    if(!token) {
+      return res.status(400).json({
+        success: false,
+        msg: "User is not log in.."
+      })
     }
+
+    jwt.verify(token, process.env.Token_key, (err, decode) => {
+      if(err) {
+        console.log(err);
+        return res.status(400).json({
+          success: false,
+          msg: "Invalid token"
+        })
+      }
+      req.data = decode;
+      next(); // ✅ moved inside verify callback
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = getMessDataMiddleware
